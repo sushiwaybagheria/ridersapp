d0bc2e7361a243597339af713df73a57f390fd39
sushiwaybagheria <sushiwaybagheria@gmail.com>
2025-06-15 01:08:38 +0200

    Collegato TableList.js a Firestore per visualizzare l'elenco riders

--- src/views/TableList.js
+++ src/views/TableList.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "firebase.js";
+import db from "../firebase";
 
 // react-bootstrap components
 import {
-  Badge,
   Button,
   Card,
-  Navbar,
-  Nav,
   Table,
   Container,
   Row,
@@ -21,61 +18,60 @@ function TableList() {
   useEffect(() => {
     const fetchRiders = async () => {
       const querySnapshot = await getDocs(collection(db, "riders"));
-      const ridersData = querySnapshot.docs.map((doc, index) => ({ id: doc.id, index: index + 1, ...doc.data() }));
-      setRiders(ridersData);
+      const data = querySnapshot.docs.map((doc) => doc.data());
+      setRiders(data);
     };
+
     fetchRiders();
   }, []);
 
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col md="12">
-            <Card className="strpied-tabled-with-hover">
-              <Card.Header>
-                <Card.Title as="h4">Elenco Riders</Card.Title>
-                <p className="card-category">Tutti i rider registrati nel sistema</p>
-              </Card.Header>
-              <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover table-striped">
-                  <thead>
-                    <tr>
-                      <th className="border-0">#</th>
-                      <th className="border-0">Cognome</th>
-                      <th className="border-0">Nome</th>
-                      <th className="border-0">Età</th>
-                      <th className="border-0">Telefono</th>
-                      <th className="border-0">Mezzo</th>
-                      <th className="border-0">Disponibilità</th>
-                      <th className="border-0">Consegne</th>
-                      <th className="border-0">Note</th>
-                      <th className="border-0">Data Reg.</th>
+    <Container fluid>
+      <Row>
+        <Col md="12">
+          <Card className="strpied-tabled-with-hover">
+            <Card.Header>
+              <Card.Title as="h4">Elenco Riders</Card.Title>
+              <p className="card-category">Tutti i rider registrati nel sistema</p>
+            </Card.Header>
+            <Card.Body className="table-full-width table-responsive px-0">
+              <Table className="table-hover table-striped">
+                <thead>
+                  <tr>
+                    <th className="border-0">#</th>
+                    <th className="border-0">Cognome</th>
+                    <th className="border-0">Nome</th>
+                    <th className="border-0">Età</th>
+                    <th className="border-0">Telefono</th>
+                    <th className="border-0">Mezzo</th>
+                    <th className="border-0">Disponibilità</th>
+                    <th className="border-0">Consegne</th>
+                    <th className="border-0">Note</th>
+                    <th className="border-0">Data Reg.</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {riders.map((r, idx) => (
+                    <tr key={idx}>
+                      <td>{idx + 1}</td>
+                      <td>{r.cognome}</td>
+                      <td>{r.nome}</td>
+                      <td>{r.eta}</td>
+                      <td>{r.telefono}</td>
+                      <td>{r.mezzo}</td>
+                      <td>{r.disponibilita}</td>
+                      <td>{r.numero_consegne}</td>
+                      <td>{r.note}</td>
+                      <td>{r.data_reg}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {riders.map((rider) => (
-                      <tr key={rider.id}>
-                        <td>{rider.index}</td>
-                        <td>{rider.COGNOME}</td>
-                        <td>{rider.NOME}</td>
-                        <td>{rider.ETA}</td>
-                        <td>{rider.TELEFONO}</td>
-                        <td>{rider.MEZZO}</td>
-                        <td>{rider.DISPONIBILITA}</td>
-                        <td>{rider.NUMERO_CONSEGNE}</td>
-                        <td>{rider.NOTE}</td>
-                        <td>{rider.DATA_REG}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+                  ))}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
