0c10738ebcf3228df2f58090c0a12bc549f9ba87
sushiwaybagheria <sushiwaybagheria@gmail.com>
2025-06-15 11:50:18 +0200

    Fix visualizzazione ordini e collegamento pulsante 'Aggiungi Ordine'

--- src/views/OrderForm.js
+++ src/views/OrderForm.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { addDoc, collection } from "firebase/firestore";
 import db from "../firebase";
-
-// react-bootstrap components
 import {
   Button,
   Card,
@@ -14,11 +12,12 @@ import {
 
 function OrderForm() {
   const [order, setOrder] = useState({
+    ID: "",
     cliente: "",
     indirizzo: "",
-    dataConsegna: "",
+    telefono: "",
     orarioConsegna: "",
-    note: ""
+    note: "",
   });
 
   const handleChange = (e) => {
@@ -31,16 +30,18 @@ function OrderForm() {
     try {
       await addDoc(collection(db, "ordini_riders"), {
         ...order,
+        assegnatoA: null,
         stato: "disponibile",
-        assegnatoA: null
+        dataConsegna: new Date().toISOString().split("T")[0], // es: "2025-06-15"
       });
       alert("Ordine aggiunto con successo!");
       setOrder({
+        ID: "",
         cliente: "",
         indirizzo: "",
-        dataConsegna: "",
+        telefono: "",
         orarioConsegna: "",
-        note: ""
+        note: "",
       });
     } catch (error) {
       console.error("Errore durante l'aggiunta dell'ordine:", error);
@@ -59,12 +60,22 @@ function OrderForm() {
             <Card.Body>
               <Form onSubmit={handleSubmit}>
                 <Row>
-                  <Col md="6">
+                  <Col md="4">
+                    <Form.Group>
+                      <label>ID</label>
+                      <Form.Control
+                        type="number"
+                        name="ID"
+                        value={order.ID}
+                        onChange={handleChange}
+                        required
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md="4">
                     <Form.Group>
                       <label>Cliente</label>
                       <Form.Control
-                        placeholder="Nome cliente"
-                        type="text"
                         name="cliente"
                         value={order.cliente}
                         onChange={handleChange}
@@ -72,30 +83,26 @@ function OrderForm() {
                       />
                     </Form.Group>
                   </Col>
-                  <Col md="6">
+                  <Col md="4">
                     <Form.Group>
-                      <label>Indirizzo</label>
+                      <label>Telefono</label>
                       <Form.Control
-                        placeholder="Indirizzo di consegna"
-                        type="text"
-                        name="indirizzo"
-                        value={order.indirizzo}
+                        name="telefono"
+                        value={order.telefono}
                         onChange={handleChange}
-                        required
                       />
                     </Form.Group>
                   </Col>
                 </Row>
+
                 <Row>
                   <Col md="6">
                     <Form.Group>
-                      <label>Data Consegna</label>
+                      <label>Indirizzo</label>
                       <Form.Control
-                        type="date"
-                        name="dataConsegna"
-                        value={order.dataConsegna}
+                        name="indirizzo"
+                        value={order.indirizzo}
                         onChange={handleChange}
-                        required
                       />
                     </Form.Group>
                   </Col>
@@ -103,23 +110,19 @@ function OrderForm() {
                     <Form.Group>
                       <label>Orario Consegna</label>
                       <Form.Control
-                        placeholder="Es. 21:00"
-                        type="time"
                         name="orarioConsegna"
                         value={order.orarioConsegna}
                         onChange={handleChange}
-                        required
                       />
                     </Form.Group>
                   </Col>
                 </Row>
+
                 <Row>
                   <Col md="12">
                     <Form.Group>
                       <label>Note</label>
                       <Form.Control
-                        placeholder="Note aggiuntive"
-                        type="text"
                         name="note"
                         value={order.note}
                         onChange={handleChange}
@@ -127,6 +130,7 @@ function OrderForm() {
                     </Form.Group>
                   </Col>
                 </Row>
+
                 <Button className="btn-fill pull-right" type="submit" variant="info">
                   Salva Ordine
                 </Button>
